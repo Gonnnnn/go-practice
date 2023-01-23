d8ba9900892ad17decb8234aaf0a90d63e03a2c1
Gon Kim <geonkim23@gmail.com>
2023-01-24 00:32:30 +0900

    Server main.html and Add Manager

--- main.go
+++ main.go
@@ -1,28 +1,47 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+type Manager struct {
+	template *template.Template
+}
+
+func newManager() *Manager {
+	return &Manager{
+		template: template.Must(template.ParseGlob("html/*.html")),
+	}
+}
+
+func (manager *Manager) run() {
+	http.HandleFunc("/", manager.rootHandler)
+	http.HandleFunc("/select", manager.selectHandler)
+	http.HandleFunc("/insert", manager.insertHandler)
+	http.HandleFunc("/delete", manager.deleteHandler)
+	http.ListenAndServe(":8000", nil)
+}
 
 func main(){
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/select", selectHandler)
-	http.HandleFunc("/insert", insertHandler)
-	http.HandleFunc("/delete", deleteHandler)
-	http.ListenAndServe(":8000", nil)
-
+	newManager().run()
 }
 
-func rootHandler(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("hi"))
+func (manager *Manager) rootHandler(writer http.ResponseWriter, request *http.Request) {
+	executeError := manager.template.ExecuteTemplate(writer, "main.html", nil)
+	if executeError != nil {
+		writer.Write([]byte("error"))
+	}
 }
 
-func selectHandler(writer http.ResponseWriter, request *http.Request) {
+func (manager *Manager) selectHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte("selectHandler was called"))
 }
 
-func insertHandler(writer http.ResponseWriter, request *http.Request) {
+func (manager *Manager) insertHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte("insertHandler was called"))
 }
 
-func deleteHandler(writer http.ResponseWriter, request *http.Request) {
+func (manager *Manager) deleteHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte("deleteHandler was called"))
 }
